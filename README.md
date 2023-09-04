c7268a9bb7de7b1f1a50b492f61aca476dbcc22a
Gaurav <gauravs@byteridge.com>
2023-09-04 12:01:35 +0530

    update repo

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# react-redux-registration-login-example
 
-React + Redux - User Registration and Login Tutorial & Example
+# react-18-redux-registration-login-example
 
-For documentation and further details go to http://jasonwatmore.com/post/2017/09/16/react-redux-user-registration-and-login-tutorial-example
+
--- src/_components/PrivateRoute.jsx
+++ src/_components/PrivateRoute.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        localStorage.getItem('user')
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )} />
-)
+import { history } from '_helpers';
+
+export { PrivateRoute };
+
+function PrivateRoute() {
+    const auth = useSelector(x => x.auth.value);
+
+    if (!auth) {
+        // not logged in so redirect to login page with the return url
+        return <Navigate to="/account/login" state={{ from: history.location }} />
+    }
+
+    // authorized so return outlet for child routes
+    return <Outlet />;
+}
